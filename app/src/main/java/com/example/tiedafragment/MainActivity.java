3168a6e3166b47ece93da731fbf94616411b99e2
ArturoBorja <53122722+ArturoBorja@users.noreply.github.com>
2019-10-14 22:03:19 -0500

    funciona en tablets
    se aumento el funcionamiento en pantalla dibidida para tablets

--- app/src/main/java/com/example/tiedafragment/MainActivity.java
+++ app/src/main/java/com/example/tiedafragment/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.tiedafragment;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements ListaCategoriaFragment.CallBackCategoria {
+    DetalleFragment detalleFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +19,23 @@ public class MainActivity extends AppCompatActivity implements ListaCategoriaFra
         FragmentManager fm = getSupportFragmentManager();
         ListaCategoriaFragment lcf = (ListaCategoriaFragment) fm.findFragmentById(R.id.frg_main);
         lcf.categoriaCallback = this;
+        if (isTablet(this)){
+            detalleFragment=(DetalleFragment) fm.findFragmentById(R.id.frg_main_detalle);
+        }
     }
 
     @Override
     public void CategoriaSeleccionada(int codigo) {
-        Toast.makeText(this,"cod+ "+codigo,Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(MainActivity.this, DetalleActivity.class);
-        intent.putExtra("codigo",codigo);
-        startActivity(intent);
+        if(isTablet(this)){
+            detalleFragment.CargarDatos(codigo);
+        }else{
+            Toast.makeText(this,"cod+ "+codigo,Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(MainActivity.this, DetalleActivity.class);
+            intent.putExtra("codigo",codigo);
+            startActivity(intent);
+        }
+    }
+    public static boolean isTablet (Context context){
+        return (context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK)>=Configuration.SCREENLAYOUT_SIZE_LARGE;
     }
 }
